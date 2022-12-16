5833b6f287ebdf041ec1d10fac3ac1227a687ba5
uluserdar <serdar.uludag@hotmail.com>
2022-12-16 16:43:36 +0300

    Refactoring

---
+++ IpRangeCalculator/Helpers/CsvStringDataHelper.cs
@@ -0,0 +1,65 @@
+﻿using NetTools;
+using SimpleConsoleProgress;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpRangeCalculator.Helpers
+{
+    public static class CsvStringDataHelper
+    {
+        public static async Task CreateIpAddressListAsync(this StringBuilder csvStringData, string fileName)
+        {
+            await Task.Run(() =>
+            {
+                string[] lines = new string[0];
+                if (File.Exists(fileName)) lines = File.ReadAllLines(fileName);
+                else throw new FileNotFoundException("File not found!");
+                Console.WriteLine($":Total IP Range {lines.Length}");
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    IPAddressRange ipAddressRange = new IPAddressRange();
+                    IPAddressRange.TryParse(lines[i], out ipAddressRange);
+                    if (ipAddressRange != null)
+                    {
+                        Console.WriteLine($"Processing IP range: {i+1}/{lines.Length}");
+                        Console.WriteLine($"IP Range: {ipAddressRange.ToCidrString()} - Start IP: {ipAddressRange.Begin} - End IP: {ipAddressRange.End}");
+                        Console.WriteLine($"{ipAddressRange.ToCidrString()} processing...");
+                        int ipAddressIndex = 0;
+                        int ipAddressCount = ipAddressRange.AsEnumerable().Count();
+                        foreach (var ipAddress in ipAddressRange)
+                        {
+                            if (lines[i].Contains("/") && (ipAddress.ToString() == ipAddressRange.Begin.ToString() || ipAddress.ToString() == ipAddressRange.End.ToString()))
+                                continue;
+                            csvStringData.AppendLine($"{lines[i]};{ipAddress.ToString()}");
+                            ipAddressIndex++;
+                            Progress.Write(ipAddressIndex, ipAddressCount);
+                            GC.Collect();
+                        }
+                        Console.WriteLine($"{ipAddressRange.ToCidrString()} done");
+                    }
+                }
+            });
+        }
+        public static async Task CreateCsvFileAsync(this StringBuilder csvStringData, string fileName)
+        {
+            await Task.Run(() =>
+            {
+                string csvFileName = $"{Path.GetFileNameWithoutExtension(fileName)}.csv";
+
+                if (File.Exists(csvFileName)) File.Delete(csvFileName);
+
+                using (FileStream fs = new FileStream(csvFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        byte[] buffer = Encoding.UTF8.GetBytes(csvStringData.ToString());
+                        fs.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            });
+        }
+    }
+}
--- IpRangeCalculator/Program.cs
+++ IpRangeCalculator/Program.cs
@@ -1,13 +1,6 @@
-﻿using NetTools;
-using Newtonsoft.Json;
-using SimpleConsoleProgress;
+﻿using IpRangeCalculator.Helpers;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.NetworkInformation;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace IpRangeCalculator
@@ -16,84 +9,37 @@ namespace IpRangeCalculator
     {
         static void Main(string[] args)
         {
+            IpAddressToCsv().Wait();
+        }
+
+        static async Task IpAddressToCsv()
+        {
             string[] lines = new string[0];
+            StringBuilder csvStringData = new StringBuilder();
 
             string fileName;
             while (true)
             {
-                Console.WriteLine("Dosya adını giriniz.");
+                Console.WriteLine("Enter file name.");
                 fileName = Console.ReadLine();
 
-                lines = GetTextLines(fileName);
-                if (lines.Length == 0)
+                csvStringData.AppendLine("Ip Range;Ip Address");
+                try
                 {
-                    Console.WriteLine("Dosa bulunamadı!");
+                    await csvStringData.CreateIpAddressListAsync(fileName);
+                    await csvStringData.CreateCsvFileAsync(fileName);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
                     continue;
                 }
 
-                List<IpRangeResult> ipRangeResults = new List<IpRangeResult>();
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    GetIpRangeResults(ipRangeResults, lines[i]).GetAwaiter();
-                    Progress.Write(i, lines.Length);
-                }
-
-                 
-                CreateCsvFile(ipRangeResults, fileName);
-                Console.WriteLine("Csv dosyası oluşturuldu");
-                Console.WriteLine("Programı kapatmak için bir tuşa basınız");
+                Console.WriteLine("CSV file created.");
+                Console.WriteLine("Press any key to close the program");
                 Console.ReadKey();
                 break;
-            }
-        }
-
-        public static string[] GetTextLines(string fileName)
-        {
-            if (File.Exists(fileName))
-            {
-                return File.ReadAllLines(fileName);
-            }
-            return new string[0];
-        }
-        public static async Task GetIpRangeResults(List<IpRangeResult> ipRangeResults, string line)
-        {
-            IPAddressRange ipAddressRange = new IPAddressRange();
-            IPAddressRange.TryParse(line, out ipAddressRange);
-            if (ipAddressRange != null)
-                foreach (var ipAddress in ipAddressRange)
-                {
-                    if (line.Contains("/") && (ipAddress.ToString() == ipAddressRange.Begin.ToString() || ipAddress.ToString() == ipAddressRange.End.ToString()))
-                        continue;
-
-                    ipRangeResults.Add(
-                        new IpRangeResult()
-                        {
-                            IpRange = line,
-                            IpAddress = ipAddress.ToString()
-                        }
-                        );
-                    GC.Collect();
-                }
-
-            await Task.Delay(0);
-        }
-        public static void CreateCsvFile(List<IpRangeResult> ipRangeResults, string fileName)
-        {
-            string jsonData = JsonConvert.SerializeObject(ipRangeResults);
-
-            string csvString = CsvConvertHelper.JsonToCSV(jsonData, ";");
-
-            string csvFileName = $"{Path.GetFileNameWithoutExtension(fileName)}.csv";
-
-            if (File.Exists(csvFileName)) File.Delete(csvFileName);
-
-            using (FileStream fs = new FileStream(csvFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                using (StreamWriter sw = new StreamWriter(fs))
-                {
-                    byte[] buffer = Encoding.UTF8.GetBytes(csvString);
-                    fs.Write(buffer, 0, buffer.Length);
-                }
             }
         }
     }
